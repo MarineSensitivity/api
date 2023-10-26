5ef2003cbb018e3883c66155b704d9f13434b12a
Ben Best <ben@ecoquants.com>
2023-10-26 17:03:17 +0000

    init API w/ Plumber default endpoints closes #1

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# api
-application programming interface (API) using R Plumber package
+# API
+
+The custom Application Programming Interface (API) using the R package  [`plumber`](https://www.rplumber.io/).
+
+## start
+
+Run the API in the background from server's RStudio **Terminal**:
+
+```bash
+# run as root and send to background (&)
+sudo Rscript /share/github/MarineSensitivities/api/run-api.R &
+```
+
+## stop
+
+```bash
+# get the process id of the running service
+ps -eaf | grep run-api
+# root        2343    2254  0 16:58 pts/0    00:00:00 sudo Rscript /share/github/MarineSensitivities/api/run-api.R
+# admin       2378    2254  0 17:00 pts/0    00:00:00 grep --color=auto run-api
+sudo kill -9 2343
+# [1]+  Killed                  Rscript /share/github/api/run-api.R
+```
+
---
+++ plumber.R
@@ -0,0 +1,58 @@
+#
+# This is a Plumber API. In RStudio 1.2 or newer you can run the API by
+# clicking the 'Run API' button above.
+#
+# In RStudio 1.1 or older, see the Plumber documentation for details
+# on running the API.
+#
+# Find out more about building APIs with Plumber here:
+#
+#    https://www.rplumber.io/
+#
+
+library(plumber)
+
+#* @apiTitle MarineSensitivites Custom API
+
+# /echo ----
+#* Echo back the input
+#* @param msg The message to echo
+#* @get /echo
+function(msg=""){
+  list(msg = paste0("The message is: '", msg, "'"))
+}
+
+# /plot ----
+#* Plot a histogram
+#* @serializer png
+#* @get /plot
+function(){
+  rand <- rnorm(100)
+  hist(rand)
+}
+
+# /sum ----
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @param b The second number to add
+#* @post /sum
+function(a, b){
+  as.numeric(a) + as.numeric(b)
+}
+
+# / home ----
+#* redirect to the swagger interface
+#* @get /
+#* @serializer html
+function(req, res) {
+  res$status <- 303 # redirect
+  res$setHeader("Location", "./__swagger__/")
+  "<html>
+  <head>
+    <meta http-equiv=\"Refresh\" content=\"0; url=./__swagger__/\" />
+  </head>
+  <body>
+    <p>For documentation on this API, please visit <a href=\"http://api.ships4whales.org/__swagger__/\">http://api.ships4whales.org/__swagger__/</a>.</p>
+  </body>
+</html>"
+}
